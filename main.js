56638d5bbcb7eed2170e9dafb426c59d29dc194d
kayleoss <katie_acting@live.ca>
2017-08-04 14:14:06 -0400

    animation send

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ function main() {
     $('.send').delay(5200);
     $('.send').fadeIn(1500);
     $('.send').on('click', function(){
+        $('.send').fadeOut(100);
         $('.third').fadeIn(500);
         $('.fourth').delay(1000);
         $('.fourth').fadeIn(500);
+        $('.send').delay(2000);
+        $('.send').fadeIn(1000);
             $('.send').on('click',function(){
                 $('.fifth').fadeIn(500);
-                $('.sixth').delay(3000);
+                $('.sixth').delay(2000);
                 $('.sixth').fadeIn(500);
                     $('.send').on('click', function(){
                         $('.seven').fadeIn(500);
@@ -23,11 +26,12 @@ function main() {
                                 $('.reply').delay(2000);
                                 $('.reply').fadeIn(500);
                                     $('.send').on('click',function(){
+                                        $('.send').delay(0);
+                                        $('.send').fadeOut(2000);
                                         $('.ninth').fadeIn(500);
-                                        $('.last').delay(2500);
+                                        $('.last').delay(2000);
                                         $('.last').fadeIn(500);
-                                        $('.send').delay(3500);
-                                        $('.send').fadeOut(1000);
+                                       
                                         
                                         
 
@@ -43,7 +47,7 @@ function main() {
                             })
                     })
             })
-    })
+         })
 
 
 
