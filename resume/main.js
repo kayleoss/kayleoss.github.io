2112a99a676b8c2e5c8c5fd7b68f46a3807e7475
kayleoss <katie_acting@live.ca>
2017-08-11 19:11:46 -0400

    resume

---
+++ resume/main.js
@@ -0,0 +1,36 @@
+var back = document.getElementById('back');
+var head = document.getElementById('head');
+var headp = document.getElementById('headp');
+var position= 100;
+function main(){
+    $('#hidden').hide();
+    $('#hidden').show();
+    $('.mediaicons').hide();
+    $('#back').hide();
+    $('#back').show();
+    $('#head').hide();
+    $('#head').delay(1000);
+    $('#head').fadeIn(2000);
+    $('.mediaicons').delay(5500);
+    $('.mediaicons').slideDown(1000);
+    setTimeout(interval, 3000);
+
+    function interval(){
+         setInterval(moveback,10);
+    }
+
+    function moveback(){
+        
+        if (position > 0){
+            position -= 1;
+            back.style.right= position + "%";
+        }
+        if (position <= 0){
+            clearInterval(interval);
+        }
+        } //end of 'moveback'
+} //End of main function 
+
+     
+
+$('document').ready(main);
