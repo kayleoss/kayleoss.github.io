f550caec16da7c42b0b195f303bb3bb97288e966
kayleoss <katie_acting@live.ca>
2017-08-14 21:20:55 -0400

    bravo responsive

--- bravo/script.js
+++ bravo/script.js
@@ -1,24 +1,30 @@
-function main(){
+window.onload = function main(){
 	$('.locationpage').hide();
 	$('.container').hide();
+	$('.coffeetext').show();
+	$('.coffeetwotext').show();
+	$('.coffeeonetext').show();
 	$('.coffeetext').hide();
 	$('.coffeeonetext').hide();
 	$('.coffeetwotext').hide();
-	$('.coffee').hover(function(){
-		$('.coffeetext').slideDown();
-		$('.coffeetext').animate({
-			left:'+150'},200);
-		});
+	$('.coffeeone').delay(1500);
+	$('.coffeetext').slideDown();
+	$('.coffeetext').animate({
+			left:'+1%'},200);
 	$('.coffeeone').hover(function(){
 		$('.coffeeonetext').slideDown();
 		$('.coffeeonetext').animate({
-			right:'+100'},200);
+			right:'+1%'},200);
 	});
 	$('.coffeetwo').hover(function(){
 		$('.coffeetwotext').slideDown();
 		$('.coffeetwotext').animate({
-			left:'+250'},200);
+			left:'+1%'},200);
 		});
+	
+	$('.cofcof').hide();
+	$('.cofcof').delay(500);
+	$('.cofcof').slideDown(1000);
 	//About Us Page//
 	$('.about').on('click',function(){
 		$('.locationpage').hide();
@@ -27,6 +33,7 @@ function main(){
 	});
 	//Location//
 	$('.location').on('click',function(){
+	$('.locationtext').fadeIn(2000);
 	$('.container').hide();
 	$('.homepage').hide();
 	$('.locationpage').fadeIn(2000);
@@ -44,4 +51,3 @@ function main(){
 
 
 
-$(document).ready(main);
