8367bc6278a455e34e667610296b9edb86945988
kayleoss <katie_acting@live.ca>
2017-08-24 12:48:06 -0400

    hungry intro

--- main.js
+++ main.js
@@ -1,37 +1,91 @@
-const back = document.getElementById('back');
-const head = document.getElementById('head');
-const headp = document.getElementById('headp');
-let position = 100;
+const burgerImage = document.getElementsByClassName('hamburger');
+const drinkImg = document.getElementsByClassName('drink');
+const pizzaImg = document.getElementsByClassName('pizza');
+const donutImg = document.getElementsByClassName('donut');
+let slideIndex = 0;
+let slideIndex2 = 0;
+let slideIndex3 = 0;
+let slideIndex4 = 0;
 
-window.onload = function main(){
-    $('#hidden').hide();
-    $('#hidden').show();
-    $('.mediaicons').hide();
-    $('#back').hide();
-    $('#back').show();
-    $('#head').hide();
-    $('#head').delay(1000);
-    $('#head').fadeIn(2000);
-    $('.mediaicons').delay(6000);
-    $('.mediaicons').slideDown(1000);
+window.onload = function (){
+//start page hidden stuff
+    $('.hidden').show();
+    $('#intro').hide();
+    $('#foodOne').hide();
+    $('#foodTwo').hide();
+    $('#foodThree').hide();
+    $('#foodFour').hide();
+    $('#website').hide();
 
-    setTimeout(interval, 3000);
 
-    function interval() {
-         setInterval(moveback,30);
+//Intro animations//
+    $('#intro').delay(1000);
+    $('#intro').fadeIn(1000);
+    $('#foodOne').delay(2500);
+    $('#foodOne').fadeIn(500);
+    burger(); 
+
+    function burger(){
+        for(x=0; x<burgerImage.length; x++){
+            burgerImage[x].style.display = "none";
+        }
+        slideIndex++;
+        if(slideIndex > burgerImage.length){slideIndex=1}
+        burgerImage[slideIndex-1].style.display = "block";
+        setTimeout(burger, 250);
     }
-    
-   function moveback () {
-        if (position > 0){
-            position -= 5;
-            back.style.right= position + "%";
+    $('#foodOne').on('click',() => {
+        drink();
+        $('#intro').fadeOut(1000);
+        $('#foodOne').fadeOut(1000);
+        $('#foodTwo').delay(1000);
+        $('#foodTwo').fadeIn(1000);
+    })
+    function drink(){
+        for(i=0; i<drinkImg.length; i++){
+            drinkImg[i].style.display = "none";
         }
-        if (position <= 0){
-            clearInterval(interval);
+        slideIndex2++;
+        if(slideIndex2 > drinkImg.length){slideIndex2=1}
+        drinkImg[slideIndex2-1].style.display = "block";
+        setTimeout(drink, 250);
+    }
+    $('#foodTwo').on('click',() => { 
+        pizza();
+        $('#foodTwo').fadeOut(1000);
+        $('#foodThree').delay(1000);
+        $('#foodThree').fadeIn(1000);
+    })
+    function pizza(){
+        for(y=0; y<pizzaImg.length; y++){
+            pizzaImg[y].style.display = "none";
         }
-    } //end of 'moveback'
-} //End of main function 
+        slideIndex3++;
+        if(slideIndex3 > pizzaImg.length){slideIndex3=1}
+       pizzaImg[slideIndex3-1].style.display = "block";
+        setTimeout(pizza, 250);
+    }
+    $('#foodThree').on('click',() => {
+        donut();
+        $('#foodThree').fadeOut(1000);
+        $('#foodFour').delay(1000);
+        $('#foodFour').fadeIn(1000);
+    })
+    function donut(){
+        for(z=0; z<donutImg.length; z++){
+            donutImg[z].style.display = "none";
+        }
+        slideIndex4++;
+        if(slideIndex4 > donutImg.length){slideIndex4=1}
+       donutImg[slideIndex4-1].style.display = "block";
+        setTimeout(donut, 250);
+    }
+    $('#foodFour').on('click', ()=>{
+        $('#foodFour').fadeOut(1000);
+        $('#website').delay(1000);
+        $('#website').fadeIn(1000);
+    })
 
-     
 
 
+}
