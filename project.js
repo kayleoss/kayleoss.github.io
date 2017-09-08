548237bcfb2b81946b1302de4704a521ef0ab517
kayleoss <katie_acting@live.ca>
2017-09-07 22:49:55 -0400

    techstore homepage

--- project.js
+++ project.js
@@ -15,27 +15,21 @@ window.onload = function main(){
         }
     $('.hide').hide();
     $('.hide').show();
+    $('#footer').hide();
+    $('#footer').delay(600);
+    $('#footer').fadeIn(1000);
     $('#open').hide();
     $('#open').fadeIn(2000);
     $('#bravoimg').hide();
-    $('#resumeimg').hide();
     $("#game").hide();
     $('#text').hide();
-    $('#reminders').hide();
-    $('#reminders').delay(500);
-    $('#reminders').fadeIn(1000);
     $('#tales').hide();
     $('#tales').delay(500);
     $('#tales').fadeIn(1000);
-    $('#blogger').hide();
     $('#bravoimg').delay(500);
     $('#bravoimg').fadeIn(1000);
     $('#game').delay(500);
     $('#game').fadeIn(1000);
-    $('#blogger').delay(500);
-    $('#blogger').fadeIn(1000);
-    $('#resumeimg').delay(500);
-    $('#resumeimg').fadeIn(1000);
     $('#text').delay(500);
     $('#text').fadeIn(1000);
 }
