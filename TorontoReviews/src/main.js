8dd63d3ac7d08aefddfcf37415d2f65d4c65ef31
kayleoss <katie_acting@live.ca>
2017-09-13 09:37:00 -0400

    added torontoreviews

---
+++ TorontoReviews/src/main.js
@@ -0,0 +1,7 @@
+window.onload= function (){
+    $('.container-fluid').show();
+    $('.container-fluid').hide();
+    $('.container-fluid').fadeIn(1000);
+    document.getElementById('vid').play();
+
+}
