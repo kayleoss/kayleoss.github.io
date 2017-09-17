fa2f1a4209c6d48ef603faeb837ef2eb39297ebb
kayleoss <katie_acting@live.ca>
2017-09-17 16:15:59 -0400

    bravo changes

--- bravo/script.js
+++ bravo/script.js
@@ -1,48 +1,60 @@
 window.onload = function main(){
 	$('.locationpage').hide();
 	$('.container').hide();
-	$('.coffeetext').show();
-	$('.coffeetwotext').show();
-	$('.coffeeonetext').show();
 	$('.coffeetext').hide();
-	$('.coffeeonetext').hide();
-	$('.coffeetwotext').hide();
-	$('.coffeeone').delay(1500);
-	$('.coffeetext').slideDown();
+	$('.heartimg').hide();
+	$('.menuitem').hide();
+	
+	$('.coffeetext17').hide();
+	if (window.innerWidth > 700){
+	$('.coffeetext17').delay(1500);
+	$('.coffeetext17').fadeIn();
+	$('.coffeetext17').animate({
+		left: '65%', top: '85%'}, 1000);
+	$('.coffeetext').fadeIn();
+	$('.heartimg').delay(2500);
+	$('.heartimg').fadeIn();
+	$('.heartimg').animate({
+		left:'80%', top:'25%'
+	}, 500);
+	$('.heartimg2').delay(3000);
+	$('.heartimg2').fadeIn();
+	$('.heartimg2').animate({
+		left:'75%', top:'45%'
+	}, 500);
+}
+	if (window.innerWidth < 700){
+	$('.coffeetext').slideDown();}
 	$('.coffeetext').animate({
-			left:'+1%'},200);
-	$('.coffeeone').hover(function(){
-		$('.coffeeonetext').slideDown();
-		$('.coffeeonetext').animate({
-			right:'+1%'},200);
-	});
-	$('.coffeetwo').hover(function(){
-		$('.coffeetwotext').slideDown();
-		$('.coffeetwotext').animate({
-			left:'+1%'},200);
-		});
+			left:'5%'},1000);
 	
 	$('.cofcof').hide();
 	$('.cofcof').delay(500);
 	$('.cofcof').slideDown(1000);
+	
 	//About Us Page//
 	$('.about').on('click',function(){
+		$('.menuitem').hide();
 		$('.locationpage').hide();
 		$('.homepage').fadeOut(1000);
 		$('.container').fadeIn(2000);
 	});
 	//Location//
 	$('.location').on('click',function(){
+	$('.menuitem').hide();
 	$('.locationtext').fadeIn(2000);
 	$('.container').hide();
 	$('.homepage').hide();
 	$('.locationpage').fadeIn(2000);
 	});
-	
-	
-
-
-	}
+	//menu//
+	$('.menu').on('click', function(){
+	$('.menuitem').show();
+	$('.locationpage').hide();
+	$('.container').hide();
+	$('.homepage').hide();
+	})
+}
 
 
 
