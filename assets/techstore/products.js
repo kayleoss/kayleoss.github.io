fd9e966bcad6e3f9b8f3deb695e4b7fda64bb877
Duo Liu <DuoLiu1@Duos-MacBook-Pro-2.local>
2017-09-25 17:23:12 -0400

    knknk

---
+++ assets/techstore/products.js
@@ -0,0 +1,40 @@
+const postText = document.querySelectorAll('.col-sm-6 p');
+const buyButton = document.querySelectorAll('.buyNow');
+window.onload=function(){
+   $('.menu-link').bigSlide();
+   $('.noshow').show();
+   if (window.innerWidth < 500){
+        var i;
+        var x;
+        for (i=0; i < postText.length;i++){
+            postText[i].style.display="none";
+        }
+        for (x=0; x < buyButton.length; x++){
+            buyButton[x].style.width = 30 + "%";
+        }
+    }else{
+        for (x=0; x < buyButton.length; x++){
+            buyButton[x].style.width = 20 + "%";
+    }
+   }
+   $('#all').on('click',function(){
+       $('.computer').fadeIn(1000);
+       $('.other').fadeIn(1000);
+       $('.mobile').fadeIn(1000);
+   })
+   $('#computer').on('click',function(){
+       $('.mobile').fadeOut();
+       $('.other').fadeOut();
+       $('.computer').fadeIn(1000);
+   })
+   $('#other').on('click', function(){
+       $('.mobile').fadeOut();
+       $('.computer').fadeOut();
+       $('.other').fadeIn(1000);
+   })
+   $('#mobile').on('click',function(){
+    $('.computer').fadeOut();
+    $('.other').fadeOut();
+    $('.mobile').fadeIn(1000);
+   })
+}
